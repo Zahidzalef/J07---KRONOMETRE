1aeb8b9c4193d3a89b84622c16e9fe8ecab9d79d
Zahid İyigökler <iyigoklerzahid55@gmail.com>
2024-08-18 14:37:35 +0300

    Files have been simplified.

--- app/src/main/java/com/zahidiyigokler/a09_basit_kronometre/MainActivity.java
+++ app/src/main/java/com/zahidiyigokler/a09_basit_kronometre/MainActivity.java
@@ -3,39 +3,27 @@ package com.zahidiyigokler.a09_basit_kronometre;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import android.os.Handler;
-import android.support.v4.app.*;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     TextView textView;
     int number;
     Runnable runnable;
     Handler handler;
     Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         textView = findViewById(R.id.textView);
         button = findViewById(R.id.button1);
         number = 0;
     }
-
     public void start (View view) {
         handler = new Handler();
         runnable = new Runnable() {
@@ -50,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
         handler.post(runnable);
         button.setEnabled(false);
     }
-
     public void stop (View view) {
         button.setEnabled(true);
         handler.removeCallbacks(runnable);
